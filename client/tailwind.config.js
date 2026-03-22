9cb3203656d9869f0e2d4828ecbbfbe1fe8fdd77
unknown <meejayproduction@gmail.com>
2026-03-22 17:54:47 +0100

    Regular update [dev]

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,4 @@
-import type { Config } from 'tailwindcss';
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
@@ -42,4 +41,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
